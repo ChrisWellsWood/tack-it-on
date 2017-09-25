6f052cc45c5b00dd1d36bf9a4f30dda9cd6b5d32
Chris Wells Wood <cwwoodesq@gmail.com>
2017-09-25 23:37:30 +0100

    Adds check for .tacked in parent dirs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate glob;
 
 use std::error::Error;
-use std::process;
+use std::path::{Path, PathBuf};
 
 use glob::glob;
 
@@ -9,18 +9,54 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     match config.mode {
         Mode::Init => run_init(),
     }
+}
+
+fn run_init() -> Result<(), Box<Error>> {
+    let cwd = Path::new(".").canonicalize()?;
+    println!("Tacking notes onto {:?}...", cwd);
+    let parent_tacked = find_tacked_notes(cwd)?;
+    if let Some(dir) = parent_tacked {
+        println!("Found tacked notes directory in {:?}", dir);
+    }
 
     Ok(())
 }
 
-fn run_init() {
-    println!("Tacking notes onto this directory...");
-    for entry in glob("*").expect("Failed to read glob pattern.") {
-        match entry {
-            Ok(path) => println!("{:?}, is file {}", path.display(), path.is_file()),
-            Err(e) => println!("{:?}", e),
+fn find_tacked_notes(dir: PathBuf) -> Result<Option<PathBuf>, Box<Error>> {
+    let path_chain = paths_from_crawl(dir);
+    for path in path_chain.iter() {
+        let found_notes = contains_notes(path);
+        if found_notes {
+            return Ok(Some(path.clone()));
         }
     }
+
+    Ok(None)
+}
+
+fn paths_from_crawl(dir: PathBuf) -> Vec<PathBuf> {
+    let mut comp_path = PathBuf::new();
+    let mut path_chain: Vec<PathBuf> = Vec::new();
+    for component in dir.components() {
+        comp_path.push(component.as_os_str());
+        path_chain.push(comp_path.clone());
+    }
+    path_chain.reverse();
+
+    path_chain
+}
+
+fn contains_notes(dir: &PathBuf) -> bool {
+    let glob_str = format!("{}/*", dir.to_str().unwrap());
+    for entry in glob(&glob_str).expect("Failed to read glob pattern.") {
+        if let Ok(path) = entry {
+           if path.ends_with(".tacked") {
+               return true;
+           }
+        }
+    }
+
+    false
 }
 
 #[derive(Debug)]
