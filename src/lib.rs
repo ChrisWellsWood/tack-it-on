938f460fafa8186c56d0db0522ed2da01a8a009a
Chris Wells Wood <cwwoodesq@gmail.com>
2017-09-26 23:14:24 +0100

    Adds user query for init if .tacked found in parent dir.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 //! Core functionality of `tack-it-on`, a project centric note taking app.
 
 extern crate glob;
+#[macro_use] extern crate text_io;
 
 use std::error::Error;
+use std::fs;
 use std::path::{Path, PathBuf};
 
 use glob::glob;
@@ -21,16 +23,23 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 fn run_init() -> Result<(), Box<Error>> {
     let cwd = Path::new(".").canonicalize()?;
     println!("Tacking notes onto {:?}...", cwd);
-    let parent_tacked = find_tacked_notes(cwd)?;
+    let parent_tacked = find_tacked_notes(&cwd)?;
+    let mut continue_init = true;
     if let Some(dir) = parent_tacked {
-        println!("Found tacked notes directory in {:?}", dir);
+        continue_init = query_init(&cwd, &dir)?;
+    }
+    if continue_init {
+        create_tacked(&cwd)?;
+        println!("Created `.tacked` in {:?}.", cwd);
+    } else {
+        println!("Did not initialise tacked notes.");
     }
 
     Ok(())
 }
 
 /// Crawls up file tree to root looking for a `.tacked` directory.
-fn find_tacked_notes(dir: PathBuf) -> Result<Option<PathBuf>, Box<Error>> {
+fn find_tacked_notes(dir: &PathBuf) -> Result<Option<PathBuf>, Box<Error>> {
     let path_chain = paths_from_crawl(dir);
     for path in path_chain.iter() {
         let found_notes = contains_notes(path);
@@ -46,7 +55,7 @@ fn find_tacked_notes(dir: PathBuf) -> Result<Option<PathBuf>, Box<Error>> {
 ///
 /// The vector of directories will be returned with the uppermost directory
 /// first and the root directory last.
-fn paths_from_crawl(dir: PathBuf) -> Vec<PathBuf> {
+fn paths_from_crawl(dir: &PathBuf) -> Vec<PathBuf> {
     let mut comp_path = PathBuf::new();
     let mut path_chain: Vec<PathBuf> = Vec::new();
     for component in dir.components() {
@@ -70,6 +79,37 @@ fn contains_notes(dir: &PathBuf) -> bool {
     }
 
     false
+}
+
+/// Queries if initialisation of project should continue.
+fn query_init(cwd: &PathBuf, tacked_loc: &PathBuf) -> Result<bool, String> {
+    if cwd == tacked_loc {
+        return Err(String::from("Current directory already has notes tacked on."));
+    }
+    println!("Found tacked notes in parent directory {:?}", tacked_loc);
+    println!("Do you want to start a new project in {:?} anyway? y/n", cwd);
+    let mut response: String;
+    let mut opt_init: Option<bool>;
+    loop {
+        response = read!("{}\n");
+        opt_init = match &*response {
+            "y" | "yes" => Some(true),
+            "n" | "no" => Some(false),
+            _ => None,
+        };
+        if let Some(_) = opt_init {
+            break;
+        }
+    }
+
+    Ok(opt_init.unwrap())
+}
+
+fn create_tacked(cwd: &PathBuf) -> Result<(), std::io::Error> {
+    let mut tacked_path = cwd.clone();
+    tacked_path.push(".tacked");
+
+    fs::create_dir(tacked_path)
 }
 
 /// Contains processed arguments for running `tack-it-on`.
