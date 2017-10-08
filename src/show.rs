7f7debaed2bf734e8c256efa47a28a184bce4aa6
Chris Wells Wood <cwwoodesq@gmail.com>
2017-10-08 22:40:09 +0100

    Breaks out show_notes fn.

--- src/show.rs
+++ src/show.rs
@@ -1,7 +1,7 @@
 //! This module contains functions for showing notes.
 
 use std::error::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap;
 
@@ -13,19 +13,26 @@ pub fn run_show(input: &clap::ArgMatches) -> Result<(), Box<Error>> {
     let cwd = Path::new(".").canonicalize()?;
     let maybe_tacked = find_tacked_notes(&cwd)?;
     if let Some(tacked_dir) = maybe_tacked {
-        let (_, notes) = get_notes(&tacked_dir)?;
-        for note in notes.iter() {
-            if let Some(ref on_file) = note.on {
-                println!("[{}] {}\nOn {}: {}\n", &note.gen_id()[..8], note.datetime,
-                         on_file.display(), note.content);
-            } else {
-                println!("[{}] {}\n{}\n", &note.gen_id()[..8], note.datetime,
-                         note.content);
-            }
-        }
+        show_notes(&tacked_dir);
     } else { 
         return Err(From::from(
             "No `.tacked` directory found. Run `init` before adding notes."));
+    }
+
+    Ok(())
+}
+
+/// Shows all notes.
+fn show_notes(tacked_dir: &PathBuf) -> Result<(), Box<Error>> {
+    let (_, notes) = get_notes(tacked_dir)?;
+    for note in notes.iter() {
+        if let Some(ref on_file) = note.on {
+            println!("[{}] {}\nOn {}: {}\n", &note.gen_id()[..8], note.datetime,
+                     on_file.display(), note.content);
+        } else {
+            println!("[{}] {}\n{}\n", &note.gen_id()[..8], note.datetime,
+                     note.content);
+        }
     }
 
     Ok(())
